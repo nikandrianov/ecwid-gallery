1d9f8e882003d976435e7819571288caf5d2475c
Nikita Andrianov <diablo3nekit@mail.ru>
2020-07-02 20:07:21 +0400

    fix end

--- src/App.js
+++ src/App.js
@@ -1,39 +1,67 @@
 import React, { Component } from 'react';
 import './App.scss';
 import axios from 'axios';
-import FormImg from './component/FormImg';
+
+const api = axios.create({
+  baseURL: `http://localhost:3000/galleryImages/`
+})
 
 class App extends Component {
-  constructor(props){
-    super(props);
+  constructor(){
+    super();
     this.state = {
       data: {},
       isFetching: true,
+      url: "",
+    };
+    this.getImgList();
+  }
+
+  getImgList = async () => {
+    api.get('/').then(resp => {
+      this.setState({data: resp.data, isFetching: false});
+    })
+  }
+
+  changeHandler = e => {
+    this.setState({url: e.target.value});
+  }
+
+  submitHandler = async e => {
+    e.preventDefault();
+    if (this.state.url === "") {
+      alert("Введите URL адрес");
+    }
+    else {
+      await api.post('/', {url: this.state.url, width: "", height: ""});
+      this.setState({url: ""});
+      this.getImgList();
     }
   }
 
-  componentDidMount() {
-    // fetch(`gallery-images.json`)
-    //   .then(resp => resp.json())
-    //   .then(result => this.setState({data: result, isFetching: false }))
-    axios.get(`http://localhost:3000/galleryImages`)
-      .then(resp => this.setState({data: resp.data, isFetching: false}))
-  }
-
-  showImg () {
-    return this.state.data.map( (item,index) => <div key={index}><img src={item.url} alt=""/></div>)
+  delImg = async (id) => {
+    await api.delete(`/${id}`);
+    this.getImgList();
   }
 
   render() {
-    const { isFetching } = this.state;
+    const { isFetching, url } = this.state;
     if (isFetching) return <div>...Loading</div>;
     return (
         <div className="wrapper">
           <div className="container">
-            <FormImg showImg={this.showImg(this.state.data)}/>
+            <div className="upload">
+              <div className="upload__content">
+                <h1>Галерея изображений</h1>
+                <form className="form-upload" onSubmit={this.submitHandler}>
+                  <input className="upload__input" value={url} onChange={this.changeHandler} placeholder="URL-адрес картинки"/>
+                  <button className="upload__button" type="submit">Загрузить</button>
+                </form>
+              </div>
+            </div>
             <div className="content">
               <div className="content__gallery">
-                {this.showImg()}
+                {this.state.data.map( item => <div key={item.id}><img src={item.url} alt=""/><button className="content__button" onClick={() => this.delImg(item.id)}>X</button></div>)}
               </div>
             </div>
           </div>
